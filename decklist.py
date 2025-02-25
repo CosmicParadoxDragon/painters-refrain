f40893782e81d279ef86fb8b9778024c4335cd1d
CosmicParadoxDragon <sohrab.kazak@gmail.com>
2025-02-24 16:56:07 -0800

    0.0.3

--- decklist.py
+++ decklist.py
@@ -9,28 +9,27 @@ class Decklist:
         self.import_deck()
 
     def import_deck(self):
-        if not os.path.isdir(self.deck_file_path):
+        if not os.path.isdir("testing/decklists"):
             raise ValueError(f"Decks directory does not exist: {self.deck_file_path}")
 
         if not os.path.exists(self.deck_file_path):
             raise ValueError(f"Deck file does not exist in: {self.deck_file_path}")
-        try:
-            # Here we are just going to fetch the card names
-            with open(self.deck_file_path, "r") as deck_file:
-                for line in deck_file:
-                    post_split = line.strip().split()
-                    set_tag_index = 2
-                    for i in range(set_tag_index, len(post_split)):
-                        if "(" in post_split[i]:
-                            set_tag_index = i
+        
+        # Here we are just going to fetch the card names
+        with open(self.deck_file_path, 'r') as deck_file:
+            for line in deck_file:
+                post_split = line.strip().split()
+                set_tag_index = 2
+                for i in range(set_tag_index, len(post_split)):
+                    if "(" in post_split[i]:
+                        set_tag_index = i
 
-                    name_list = post_split[1:set_tag_index - 1]
-                    card_name = " ".join(name_list)
-                    self.cards.append(card_name)
-        except:
+                name_list = post_split[1:set_tag_index]
+                card_name = " ".join(name_list)
+                self.cards.append(card_name)
 
-    def get_decklist():
+    def get_decklist(self):
         return self.cards
     
-    def get_file_path():
+    def get_file_path(self):
         return self.deck_file_path
